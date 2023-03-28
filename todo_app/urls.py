d1633f38c2f0ddc64a5276ade2d7e3db0e619067
Yulia Borzhak <yuliaborzhak@gmail.com>
2023-03-28 17:51:54 +0200

    todo list created

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -1,2 +1,6 @@
+from django.urls import path
+from . import views
+
 urlpatterns = [
+    path("", views.ItemListView.as_view(), name="index"),
 ]
--- todo_app/views.py
+++ todo_app/views.py
@@ -5,6 +5,11 @@ from .models import ToDoItem
 
 # Create your views here.
 
-class ListListView(ListView):
+class ItemListView(ListView):
     model = ToDoItem
     template_name = "todo_app/index.html"
+
+    # def get_context_data(self):
+    #     context = super().get_context_data()
+    #     context["todo_list"] = ToDoList.objects.get(id=self.kwargs["list_id"])
+    #     return context
