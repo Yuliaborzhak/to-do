5274cf469422f76645175600922bae706de4dcf9
Yulia Borzhak <yuliaborzhak@gmail.com>
2023-03-29 22:36:13 +0200

    static added

---
+++ todo_app/static/scripts.js
@@ -0,0 +1,20 @@
+// document.addEventListener("DOMContentLoaded", (event) => {
+//     let itemsArr = [...document.querySelectorAll(".list__item")];
+
+//     itemsArr.forEach(item => {
+//         let toggle = item.querySelector('.list__item-toggle');
+
+//         if (item.classList.contains('completed')) {
+//             toggle.setAttribute('checked')
+//         }
+
+//         toggle.addEventListener('click', e => {
+//             toggle.classList.toggle('completed')
+//             // if (toggle.hasAttribute('checked')) {
+
+//             // }
+
+//         })
+      
+//     });
+// });
